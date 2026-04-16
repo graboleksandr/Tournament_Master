b39585005131e45bb03fff2045b7e6b92af21b6f
grabsasha66@gmail.com <grabsasha66@gmail.com>
2026-04-16 12:20:07 +0300

    Add project files.

---
+++ Tournament_Master/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Tournament_Master
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
